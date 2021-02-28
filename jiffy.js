ad78abb7b192ac0a70d78eadb042b39308c6b4a7
peanutooo <48094822+peanutooo@users.noreply.github.com>
2021-02-28 10:56:25 +0800

    Added Maps Embed API

--- jiffy.js
+++ jiffy.js
@@ -21,11 +21,37 @@ $(document).ready(function() {
     });
 });
 
+
+function showMap() {
+    var trackingID = document.getElementById("trackingID").value;
+    if (trackingID.toUpperCase() == "JIEX008") {
+        document.getElementById("map").style.visibility = "visible";
+    } else if (trackingID =="") {
+        alert("Please fill in the tracking ID.");
+    }
+    else alert("This tracking ID does not match our database! Please try again or contact us for assistance.");
+}
+
+
+function showDirections() {
+    var trackingDirectionsID = document.getElementById("trackingDirectionsID").value;
+    if (trackingDirectionsID.toUpperCase() == "JIEX008") {
+        document.getElementById("directionsMap").style.visibility = "visible";
+    } else if (trackingDirectionsID =="") {
+        alert("Please fill in the tracking ID.");
+    }
+    else alert("This tracking ID does not match our database! Please try again or contact us for assistance.");
+}
+
 document.getElementById("Ampang").disabled = true;
 document.getElementById("Johor").disabled = true;
 document.getElementById("Ipoh").disabled = true;
 document.getElementById("Johor Bahru").disabled = true;
 document.getElementById("Batu Pahat").disabled = true;
+
+function getLocation() {
+    return document.getElementsByName("senderLocation")[0].value;
+}
 
 
 function calcTotalCost() {
@@ -35,6 +61,26 @@ function calcTotalCost() {
     // TBS to Taiping Terminal: 280km - BUS TICKET RM27/2 = RM13.5
     // Taiping Terminal to Taiping: 4.9km - RM7.35
 
+    itemProperty = document.getElementsByName("itemSizeWeight"); 
+    var checkedItemProperty;
+    for(i = 0; i < itemProperty.length; i++) { 
+        if(itemProperty[i].checked) 
+        {
+            checkedItemProperty = itemProperty[i].value; 
+            break;
+        }
+     }
+
+     insuranceOption = document.getElementsByName("insuranceOption"); 
+     var checkedInsuranceOption;
+     for(i = 0; i < insuranceOption.length; i++) { 
+         if(insuranceOption[i].checked) 
+         {
+            checkedInsuranceOption = insuranceOption[i].value; 
+             break;
+         }
+      }
+
     distance_senderToBusTerminal = 24.2;
     distance_terminalToTerminal = 280;
     distance_busTerminalToReceiver = 4.9;
@@ -43,11 +89,11 @@ function calcTotalCost() {
     cost_busTerminalToReceiver = BASE_FARE + (4.9 * 1.5);
     cost_terminalToTerminal = 27/2; 
     
-    if (itemProperty == 'S') parcelCost = 10.0;
-    else if (itemProperty == 'M') parcelCost = 15.0;
-    else if (itemProperty == 'L') parcelCost = 20.0;
+    if (checkedItemProperty == 'S') parcelCost = 10.0;
+    else if (checkedItemProperty == 'M') parcelCost = 15.0;
+    else if (checkedItemProperty == 'L') parcelCost = 20.0;
     
-    if (insuranceOption == 'Y') totalCost += 3.0;
+    if (checkedInsuranceOption == 'Y') totalCost += 3.0;
 
     totalCost += parcelCost + cost_senderToBusTerminal + cost_busTerminalToReceiver + cost_terminalToTerminal;
 
@@ -72,9 +118,26 @@ function showInput() {
 
 //     var lat = parseFloat(values[0]);
 //     document.getElementById('show_senderLat').innerHTML = lat;
+    
+    itemProperty = document.getElementsByName("itemSizeWeight"); 
+    for(i = 0; i < itemProperty.length; i++) { 
+        if(itemProperty[i].checked) 
+        {
+            document.getElementById("show_itemSizeWeight").innerHTML = itemProperty[i].value; 
+            break;
+        }
+     }
 
-    document.getElementById('show_itemSizeWeight').innerHTML = document.getElementsByName("itemSizeWeight")[0].value;
-    document.getElementById('show_insuranceOption').innerHTML = document.getElementsByName("insuranceOption")[0].value;
+     insuranceOption = document.getElementsByName("insuranceOption"); 
+     for(i = 0; i < insuranceOption.length; i++) { 
+         if(insuranceOption[i].checked) 
+         {
+             document.getElementById("show_insuranceOption").innerHTML = insuranceOption[i].value; 
+             break;
+         }
+      }
+
+//    document.getElementById('show_insuranceOption').innerHTML = document.getElementsByName("insuranceOption")[0].value;
     document.getElementById('show_totalCost').innerHTML = calcTotalCost();
 
 
